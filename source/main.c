3f27d7fc340e8acc27b1bf333d0addb55b6ca8ae
Robin Monjo <robin.monjo@applidget.com>
2014-03-02 21:45:45 +0100

    debugging

--- source/main.c
+++ source/main.c
@@ -381,7 +381,7 @@ uint8_t * read_mifare_ultra_light_user_data(phalMful_Sw_DataParams_t *alMful) {
         }
         fflush(stdout);
         memcpy(cursor, buffer, sizeof(buffer)); //add it to glogab buffer
-        cursor += sizeof(buffer);
+        cursor += 4 * sizeof(uint8_t);
         printf("\nAfter copy\n");
         for(idx = 0; idx < 11 * 4; idx++) {
             printf("%02X ", global_buffer[idx]);
